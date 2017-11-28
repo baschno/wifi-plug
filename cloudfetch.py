269c94808520db04416f702a1eb698726616a1a2
baschno <bastian.schnorbus@gmail.com>
2017-11-28 23:52:36 +0100

    adding tests

--- cloudfetch.py
+++ cloudfetch.py
@@ -1,18 +1,27 @@
 import json
 import urllib2
 import hashlib
+import sys
 
 
 accessKey = "Q763W08JZ07V23FR99410B3PC945LT28"
-username = urllib2.quote("icomen@schnorbus.net")
-pwd = hashlib.md5("5Tfg4s").hexdigest().upper()
-urlpref = "http://smart2connect.yunext.com"
+
 wifi_path = "/api/device/wifi/list"
 rf_path = "/api/device/rf/list"
+result = {}
 
 
-def getConfigJson(path):
-    url = '{}{}?accessKey={}&username={}&password={}'.format(urlpref, path, accessKey, username, pwd)
+def buildUrl(username, password, path):
+    urlpref = "http://smart2connect.yunext.com"
+    usr = urllib2.quote(username)
+    pwd = hashlib.md5(password).hexdigest().upper()
+    url = '{}{}?accessKey={}&username={}&password={}'.format(urlpref, path, accessKey, usr, pwd)
+
+    return url
+
+
+def getConfigJson(username, password, path):
+    url = buildUrl(username, password, path)
 
     print("url: " + url)
     response = urllib2.urlopen(url)
@@ -21,20 +30,35 @@ def getConfigJson(path):
 
 
 def get_device_name_from_item(item):
-    return item.deviceName
+    return item['deviceName']
 
 
 def get_mac_address_from_item(item):
     return item['macAddress']
 
 
-data = getConfigJson(wifi_path)
-print json.dumps(data)
-data = getConfigJson(rf_path)
-print data
+def get_code_from_item(item):
+    code = "{}{}{}".format(item['companyCode'], item['deviceType'], item['authCode'])
+    return code
 
 
-for item in data['list']:
-    print item
-    print get_device_name_from_item(item)
-    print get_mac_address_from_item(item)
+if __name__ == "__main__":
+
+    if len(sys.argv)<2:
+        print("Usage: {} <login> <password>".format(sys.argv[0]))
+        sys.exit()
+
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+
+    data = getConfigJson(usr, pwd, wifi_path)
+    for item in data['list']:
+        print item
+        result[get_device_name_from_item(item)] = "hhooo"
+
+    print result
+    data = getConfigJson(usr, pwd, rf_path)
+
+    for item in data['list']:
+        print item
+
--- test_cloudfetch.py
+++ test_cloudfetch.py
@@ -3,8 +3,25 @@ import json
 import cloudfetch
 
 
+def test_item_content():
+    test_dict = '{"deviceName": "Lampe", "macAddress": "AFFE", "orderNumber": 1, "authCode": "7150", "deviceType": "11", "companyCode": "C1"}'
+
+    test_data = json.loads(test_dict)
+
+    assert "Lampe" == cloudfetch.get_device_name_from_item(test_data)
+    assert "AFFE" == cloudfetch.get_mac_address_from_item(test_data)
+    assert "C1117150" == cloudfetch.get_code_from_item(test_data)
+
+
 def test_te():
-    test_data = json.loads('{"list": [{"deviceName": "Bodenlampe Flur", "macAddress": "ACCF2338AEF6", "orderNumber": 1, "authCode": "7150", "imageName": "1.png", "deviceType": "11", "companyCode": "C1", "lastOperation": 1475085114427}], "success": true}')
-    for item in test_data:
-        assert "Bodenlampe Flur" == cloudfetch.get_device_name_from_item(item)
-        assert "ACCF2338AEF6" == get_mac_address_from_item(item)
+    test_dict = '{"list":[{"deviceName": "Lampe", "macAddress": "AFFE", "orderNumber": 1, "authCode": "7150", "deviceType": "11", "companyCode": "C1"}]}'
+    test_data = json.loads(test_dict)
+    for item in test_data['list']:
+        print item
+        assert "Lampe" == cloudfetch.get_device_name_from_item(item)
+        assert "AFFE" == cloudfetch.get_mac_address_from_item(item)
+
+
+def test_buildUrl():
+    test_url = cloudfetch.buildUrl("u@ser", "pwd", "/path")
+    assert test_url == "http://smart2connect.yunext.com/path?accessKey=Q763W08JZ07V23FR99410B3PC945LT28&username=u%40ser&password=9003D1DF22EB4D3820015070385194C8"
