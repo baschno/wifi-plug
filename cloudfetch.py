ee1cc9ae6dcfbd0a5881b4ee1206d280c765da19
baschno <bastian.schnorbus@gmail.com>
2017-11-29 16:40:22 +0100

    finalizing the python cloudfetcher

--- cloudfetch.py
+++ cloudfetch.py
@@ -1,7 +1,13 @@
+"""
+Util for the Medion/Lidl wifi/rf-plug system.
+Connects to the Cloud Service and returns the configuration as Json file.
+"""
+
 import json
 import urllib2
 import hashlib
 import sys
+import argparse
 
 
 accessKey = "Q763W08JZ07V23FR99410B3PC945LT28"
@@ -11,7 +17,7 @@ rf_path = "/api/device/rf/list"
 result = {}
 
 
-def buildUrl(username, password, path):
+def build_url(username, password, path):
     urlpref = "http://smart2connect.yunext.com"
     usr = urllib2.quote(username)
     pwd = hashlib.md5(password).hexdigest().upper()
@@ -21,12 +27,9 @@ def buildUrl(username, password, path):
 
 
 def getConfigJson(username, password, path):
-    url = buildUrl(username, password, path)
-
-    print("url: " + url)
+    url = build_url(username, password, path)
     response = urllib2.urlopen(url)
-    data = json.load(response)
-    return data
+    return json.load(response)
 
 
 def get_device_name_from_item(item):
@@ -38,27 +41,66 @@ def get_mac_address_from_item(item):
 
 
 def get_code_from_item(item):
+    """
+    Builds the 'code' fragment out of different dict values.
+    :param item: a dict containing the needed values.
+    :return: Compiled code as string
+    """
     code = "{}{}{}".format(item['companyCode'], item['deviceType'], item['authCode'])
     return code
 
 
+def create_plug_cfg(item, companyCode=None):
+    """
+    Creates a plug configuration from the dictionary item.
+    Only the wifi plug will return the company code
+    :param item: dictionary containing the needed values.
+    :param companyCode: Optional, not returned for the rf-plugs, only for the wifi-plug.
+                        It needs to be provided from outside for the rf-plug-configs
+    :return: JSON/dictionary object containing the configuration
+    """
+    plug_cfg = {}
+    plug_cfg['mac'] = get_mac_address_from_item(item)
+
+    if 'companyCode' in item:
+            plug_cfg['code'] = get_code_from_item(item)
+
+    if not companyCode is None:
+        plug_cfg['code'] = companyCode
+
+    if 'addressCode' in item:
+            plug_cfg['rfslave'] = item['addressCode']
+
+    return plug_cfg
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--username", type=str, help="Username of Cloud Service Login", required=True)
+    parser.add_argument("--password", type=str, help="Password of Cloud Service Login", required=True)
+    parser.add_argument("--filename", type=str, help="Write the configuration to a file")
 
-    if len(sys.argv)<2:
-        print("Usage: {} <login> <password>".format(sys.argv[0]))
-        sys.exit()
-
-    usr = sys.argv[1]
-    pwd = sys.argv[2]
-
-    data = getConfigJson(usr, pwd, wifi_path)
+    args = parser.parse_args()
+    # Start with the wifi-plug path
+    data = getConfigJson(args.username, args.password, wifi_path)
     for item in data['list']:
-        print item
-        result[get_device_name_from_item(item)] = "hhooo"
 
-    print result
-    data = getConfigJson(usr, pwd, rf_path)
+        companyCode = get_code_from_item(item)
 
+        plug_cfg = create_plug_cfg(item)
+        result[get_device_name_from_item(item)] = plug_cfg
+
+    # go on with the RF-plug configuration
+    data = getConfigJson(args.username, args.password, rf_path)
     for item in data['list']:
-        print item
+        plug_cfg = create_plug_cfg(item, companyCode)
+        result[get_device_name_from_item(item)] = plug_cfg
 
+    print json.dumps(result)
+
+    if args.filename is None:
+        print("No file output")
+    else:
+        print("Output to file {}".format(args.filename))
+        with open(args.filename, "w") as outfile:
+            json.dump(result, outfile, indent=4)
--- test_cloudfetch.py
+++ test_cloudfetch.py
@@ -4,7 +4,7 @@ import cloudfetch
 
 
 def test_item_content():
-    test_dict = '{"deviceName": "Lampe", "macAddress": "AFFE", "orderNumber": 1, "authCode": "7150", "deviceType": "11", "companyCode": "C1"}'
+    test_dict = '{"deviceName": "Lampe", "macAddress": "AFFE", "orderNumber": 1, "authCode": "7150", "deviceType": "11", "addressCode": "1ADD",  "companyCode": "C1"}'
 
     test_data = json.loads(test_dict)
 
@@ -23,5 +23,5 @@ def test_te():
 
 
 def test_buildUrl():
-    test_url = cloudfetch.buildUrl("u@ser", "pwd", "/path")
+    test_url = cloudfetch.build_url("u@ser", "pwd", "/path")
     assert test_url == "http://smart2connect.yunext.com/path?accessKey=Q763W08JZ07V23FR99410B3PC945LT28&username=u%40ser&password=9003D1DF22EB4D3820015070385194C8"
